9be582e4241bd7a23ac604eace263e0903788f44
a.adamov <wof_4@mail.ru>
2022-03-28 14:37:00 +0300

    add logs

--- install.js
+++ install.js
@@ -7,22 +7,31 @@ const npm = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
 const installAll = async () => {
     const client = path.resolve('client')
     const server = path.resolve('server')
-    
+    const green = "\x1b[32m"
+    const FgMagenta = "\x1b[35m"
+    const blue = '\x1b[36m%s\x1b[0m'
+
 
     try {
-        console.log("Установка зависимостей общих файлов подождите...")
-        await execFile(npm, ["install"]);
-        console.log("Успешно выполненно")
+        console.log(blue, 'Установка зависимостей общих файлов подождите...')
+        const { stdout: mainProcess } = await execFile(npm, ["install"]);
+        console.log(FgMagenta, mainProcess)
+        console.log(green, "Успешно выполненно")
 
-        console.log("Установка зависимостей для SERVER подождите...")
-        await execFile(npm, ["install"], { cwd: server });
-        console.log("Успешно выполненно")
+        console.log(blue, 'Установка зависимостей для SERVER подождите...')
+        const { stdout: serverProcess } = await execFile(npm, ["install"], { cwd: server });
+        console.log(FgMagenta, serverProcess)
 
-        console.log("Установка зависимостей для CLIENT подождите...")
-        await execFile(npm, ["install"], { cwd: client });
-        console.log("Успешно выполненно")
+        console.log(green, "Успешно выполненно")
 
-        console.log("Выполните команду 'npm run start' для запуска приложения")
+        console.log(blue, 'Установка зависимостей для CLIENT подождите...')
+        const { stdout: clientProcess } = await execFile(npm, ["install"], { cwd: client });
+        console.log(FgMagenta, clientProcess)
+        console.log(green, "Успешно выполненно")
+
+
+
+        console.log(blue,"Выполните команду 'npm run start' для запуска приложения")
 
     } catch (err) {
         console.log(`выполнение операции завершилось ошибкой ${err}`)
